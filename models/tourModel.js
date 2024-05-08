81f0a15c0d71b44aeff7589aa6f90837115048d3
Anand Kumar Mishra <ctfu.anand@outlook.com>
2024-05-08 15:26:33 +0530

    feat: Add indexes for improved performance and search functionality

--- models/tourModel.js
+++ models/tourModel.js
@@ -122,6 +122,9 @@ const tourSchema = new mongoose.Schema(
   }
 );
 
+tourSchema.index({ price: 1, ratingsAverage: -1 });
+tourSchema.index({ slug: 1 });
+
 tourSchema.pre('save', function(next) {
   this.slug = slugify(this.name, { lower: true });
   next();
