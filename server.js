aec23ad31772eeadca05f7561951024751bf1adb
Anand Kumar Mishra <ctfu.anand@outlook.com>
2024-07-04 11:56:14 +0530

    chore: Simplify mongoose connection in server.js

--- server.js
+++ server.js
@@ -16,16 +16,9 @@ const DB = process.env.DATABASE.replace(
   process.env.DATABASE_PASSWORD
 );
 
-mongoose
-  .connect(DB, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true
-  })
-  .then(con => {
-    console.log('DB connection successful');
-  });
+mongoose.connect(DB).then(con => {
+  console.log('DB connection successful');
+});
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
